63642e8a08c12d1a4f97c877c094003ccd7733c0
Beth Schofield <bethmschofield@gmail.com>
2018-02-03 10:39:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,11 +5,10 @@ def turn_count(board)
       counter += 1
     end
     puts "It is turn #{counter}"
-  end
 end
 
   def current_player(board)
     next_player = if turn_count % 2 ? "O" : "X"
     puts next_player
   end
-  end
+end
